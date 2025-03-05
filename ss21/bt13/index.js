896499107d06a2d0e36edf98a0894a6dd6cac385
buithaison17 <125809724+buithaison17@users.noreply.github.com>
2025-03-05 20:01:09 +0700

    Add files via upload

---
+++ ss21/bt13/index.js
@@ -0,0 +1,35 @@
+// let text = '';
+// for(let i = 0; i < 10; i++){
+//     for(let j = 0; j < 10; j++){
+//         if(i + j < 9){
+//             text += '*';
+//         }
+//     }
+//     text += '<br>';
+// }
+//     document.write(text);
+
+// let text = '';
+// for(let i = 0; i < 10; i++){
+//     for(j = 0; j < 10; j++){
+//         if(i >= j){
+//             text += '*';
+//         }
+//     }
+//     text += '<br>';
+// }
+// document.write(text);
+
+// let text= '';
+// for(let i = 0; i < 10; i++){
+//     for(let j = 0; j < 10; j++){
+//         if(j > 9 - i - 1){
+//             text += '*';
+//         }
+//         else{
+//             text += ' ';
+//         }
+//     }
+//     text += '<br>';
+// }
+// document.write('<pre>' + text + '<pre>');
