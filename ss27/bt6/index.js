7807ab0efdcfe3858e474cf65a76bfaaaf29d683
buithaison17 <125809724+buithaison17@users.noreply.github.com>
2025-03-13 20:57:52 +0700

    Add files via upload

--- ss27/bt6/index.js
+++ ss27/bt6/index.js
@@ -1,13 +1,13 @@
 let arr = [3,6,9,12,15];
-let d = arr[1] - arr[0];
+let step = arr[1] - arr[0];
 
-function checkArray(arr, d){
+function checkArray(arr, step){
     if(!Array.isArray(arr) || arr.some(((value) => isNaN(value)))){
         return 'Dữ liệu không hợp lệ';
     }
     let found = true;
     for(let i = 2; i < arr.length; i++){
-        if(arr[i] !== (arr[0] + i*d)){
+        if(arr[i] !== arr[i-1] + step){
             found = false;
         }
     }
@@ -19,4 +19,4 @@ function checkArray(arr, d){
     }
 }
 
-console.log(checkArray(arr, d));
+console.log(checkArray(arr, step));
